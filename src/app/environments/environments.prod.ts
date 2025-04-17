741e3ff377618a45c717abf8b9c8a4f3ffef1909
000-dir-wmi-0001 <momindy321@gmail.com>
2025-04-17 20:14:44 +0530

    backend added

--- src/app/environments/environments.prod.ts
+++ src/app/environments/environments.prod.ts
@@ -1,6 +1,7 @@
 export const environments={
   production: true,
   // api: 'http://localhost:3000/api/auth',
-  
+  api:'https://sgtile-api.onrender.com/api/auth',
+
   geminiApi: 'AIzaSyA7rky_0L86-fsXKQtibfSRMPqMiwr5Pt4'
 }
